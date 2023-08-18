ed10e51f88c842467f98c756588e486b6aa4852d
ivarvingrencarrera <ivar.carrera@outlook.com>
2023-08-18 10:41:11 -0300

    aula 4: Atualizando e deletando recursos

--- escola/serializers.py
+++ escola/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from escola.models import Aluno, Curso
+
+from escola.models import Aluno, Curso, Matricula
+
 
 class AlunoSerializers(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +13,9 @@ class CursoSerializers(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
+
+
+class MatriculaSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from escola.serializers import AlunoSerializers, CursoSerializers
+
+from escola.models import Aluno, Curso, Matricula
+from escola.serializers import AlunoSerializers, CursoSerializers, MatriculaSerializers
+
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
@@ -11,3 +13,9 @@ class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializers
+
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """Exibindo todas as matrículas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializers
