62347b04885e3bb11d58c98694a942c35409df58
ivarvingrencarrera <ivar.carrera@outlook.com>
2023-08-18 10:04:16 -0300

    aula 2: Modelos, Admin e Serializers

---
+++ escola/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from escola.models import Aluno, Curso
+
+class AlunoSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
+    
+
+class CursoSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = '__all__'
