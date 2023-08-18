92218c008b79c0bca6a2ae241cb2397dbdb218e4
ivarvingrencarrera <ivar.carrera@outlook.com>
2023-08-18 11:01:45 -0300

    aula 5: ListAPIView e Autenticação

--- escola/views.py
+++ escola/views.py
@@ -1,21 +1,50 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
 from escola.models import Aluno, Curso, Matricula
-from escola.serializers import AlunoSerializers, CursoSerializers, MatriculaSerializers
-
+from escola.serializers import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunosSerializer, ListaAlunosMatriculados
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
     queryset = Aluno.objects.all()
-    serializer_class = AlunoSerializers
+    serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
 
 class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
-    serializer_class = CursoSerializers
+    serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class MatriculasViewSet(viewsets.ModelViewSet):
     """Exibindo todas as matrículas"""
     queryset = Matricula.objects.all()
-    serializer_class = MatriculaSerializers
+    serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as matrículas de um aluno ou aluna"""
+    serializer_class = ListaMatriculasAlunosSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando alunos e alunas matriculados em um curso"""
+    serializer_class = ListaAlunosMatriculados
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
